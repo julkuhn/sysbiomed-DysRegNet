538c7c4d93aecbf58113473b7d0cf3103d5c7a0a
laralechner <laralechner@icloud.com>
2025-01-27 13:48:55 +0100

    changed run breast.sh

--- run_dysregnet/run_dysregnet_tcga_ref_breast.sh
+++ run_dysregnet/run_dysregnet_tcga_ref_breast.sh
@@ -16,9 +16,14 @@
 # Assign paths to parameters
 EXPR="/nfs/data/patients_networks/DysRegNet_workflow/results/expression_processed/BRCA/tpm.csv"
 META="/nfs/data/patients_networks/DysRegNet_workflow/results/expression_processed/BRCA/tpm_meta.csv"
-GRN="/nfs/data/patients_networks/DysRegNet_workflow/results/reference_networks/genie3/BRCA/genie3_BRCA_tpm.top_100k.csv"
-OUT="/nfs/data2/dysregnet_gtex/results/tcga_breast_no_confounders_binary.fea"
-OUT_STATS="/nfs/data2/dysregnet_gtex/results/tcga_breast_no_confounders.csv"
+#GRN="/nfs/data/patients_networks/DysRegNet_workflow/results/reference_networks/genie3/BRCA/genie3_BRCA_tpm.top_100k.csv"
+#Felix network
+GRN="/nfs/data2/dysregnet_gtex/GENIE3_output/linkedList_output_slurm_tcga_breast_controls.csv"
+#OUT="/nfs/data2/dysregnet_gtex/results/tcga_breast_no_confounders_binary.fea"
+#OUT_STATS="/nfs/data2/dysregnet_gtex/results/tcga_breast_no_confounders.csv"
+#out felix
+OUT="/nfs/data2/dysregnet_gtex/results/tcga_refNets_Felix/tcga_breast_felix.fea"
+OUT_STATS="/nfs/data2/dysregnet_gtex/results/tcga_refNets_Felix/tcga_breast_Felix.csv"
 
 # Run the Python script with the parameters
 python run_dysregnet.py --expr $EXPR --meta $META --grn $GRN --no_direction --output $OUT --output_stats $OUT_STATS
